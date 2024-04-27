32dcc674d30c146f60700f4c0a38fb80ee449abf
lucian666 <ilhamaldizar@gmail.com>
2024-04-27 10:54:41 +0700

    update routes/api and categorytetest

--- tests/Feature/CategpryTest.php
+++ tests/Feature/CategpryTest.php
@@ -2,19 +2,58 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
+use Database\Seeders\CategorySeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CategpryTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function testResource()
     {
-        $response = $this->get('/');
+        $this->seed([CategorySeeder::class]);
 
-        $response->assertStatus(200);
+        $category = Category::first();
+
+        $this->get("/api/categories/$category->id")
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'id' => $category->id,
+                    'name' => $category->name,
+                    'created_at' => $category->created_at->toJSON(),
+                    'updated_at' => $category->updated_at->toJSON(),
+                ]
+            ]);
+
     }
+
+    public function testResourceCollection()
+    {
+        $this->seed([CategorySeeder::class]);
+
+        $categories = Category::all();
+
+        $this->get("/api/categories")
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    [
+                        'id' => $categories[0]->id,
+                        'name' => $categories[0]->name,
+                        'created_at' => $categories[0]->created_at->toJSON(),
+                        'updated_at' => $categories[0]->updated_at->toJSON(),
+                    ],
+                    [
+                        'id' => $categories[1]->id,
+                        'name' => $categories[1]->name,
+                        'created_at' => $categories[1]->created_at->toJSON(),
+                        'updated_at' => $categories[1]->updated_at->toJSON(),
+                    ]
+                ]
+            ]);
+
+    }
+
 }
